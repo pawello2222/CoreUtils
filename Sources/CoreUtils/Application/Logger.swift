fff41af408f90e5065a60a6080ed409f838a284b
pawello2222 <pawello2222@gmail.com>
2020-12-14 20:06:49 +0100

    Fixed access levels

--- Sources/CoreUtils/Application/Logger.swift
+++ Sources/CoreUtils/Application/Logger.swift
@@ -11,23 +11,23 @@ import os.log
 
 @available(iOS 10.0, *)
 public enum Logger {
-    static func log(_ message: String, category: OSLog, type: OSLogType) {
+    public static func log(_ message: String, category: OSLog, type: OSLogType) {
         os_log("%@", log: category, type: type, message)
     }
 
-    static func debug(_ message: String, category: OSLog) {
+    public static func debug(_ message: String, category: OSLog) {
         log(message, category: category, type: .debug)
     }
 
-    static func info(_ message: String, category: OSLog) {
+    public static func info(_ message: String, category: OSLog) {
         log(message, category: category, type: .info)
     }
 
-    static func error(_ message: String, category: OSLog) {
+    public static func error(_ message: String, category: OSLog) {
         log(message, category: category, type: .error)
     }
 
-    static func fault(_ message: String, category: OSLog) {
+    public static func fault(_ message: String, category: OSLog) {
         log(message, category: category, type: .fault)
     }
 }
